a1ec2943f9f48dc7e0b22024f3d6a1e5d7163d65
cristiandpto23 <cristian.web.development@gmail.com>
2024-08-27 21:34:52 -0400

    adds second - third - fourth requirements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,4 @@
-document.write('<p>Estadisticas centro medico ñuñoa</p>');
-
-var radiologia = [
-    { hora: '11:00', especialista: 'IGNACIO SCHULZ', paciente: 'FRANCISCA ROJAS', rut: '9878782-1', prevision: 'FONASA' },
-    { hora: '11:30', especialista: 'FEDERICO SUBERCASEAUX', paciente: 'PAMELA ESTRADA', rut: '15345241-3', prevision: 'ISAPRE' },
-    { hora: '15:00', especialista: 'FERNANDO WURTHZ', paciente: 'ARMANDO LUNA', rut: '16445345-9', prevision: 'ISAPRE' },
-    { hora: '15:30', especialista: 'ANA MARIA GODOY', paciente: 'MANUEL GODOY', rut: '17666419-0', prevision: 'FONASA' },
-    { hora: '16:00', especialista: 'PATRICIA SUAZO', paciente: 'RAMON ULLOA', rut: '14989389-K', prevision: 'FONASA' },
-];
-
-var traumatologia = [
-    { hora: '8:00', especialista: 'MARIA PAZ ALTUZARRA', paciente: 'PAULA SANCHEZ', rut: '15554774-5', prevision: 'FONASA' },
-    { hora: '10:00', especialista: 'RAUL ARAYA', paciente: 'ANGÉLICA NAVAS', rut: '15444147-9', prevision: 'ISAPRE' },
-    { hora: '10:30', especialista: 'MARIA ARRIAGADA', paciente: 'ANA KLAPP', rut: '17879423-9', prevision: 'ISAPRE' },
-    { hora: '11:00', especialista: 'ALEJANDRO BADILLA', paciente: 'FELIPE MARDONES', rut: '1547423-6', prevision: 'ISAPRE' },
-    { hora: '11:30', especialista: 'CECILIA BUDNIK', paciente: 'DIEGO MARRE', rut: '16554741-K', prevision: 'FONASA' },
-    { hora: '12:00', especialista: 'ARTURO CAVAGNARO', paciente: 'CECILIA MENDEZ', rut: '9747535-8', prevision: 'ISAPRE' },
-    { hora: '12:30', especialista: 'ANDRES KANACRI', paciente: 'MARCIAL SUAZO', rut: '11254785-5', prevision: 'ISAPRE' },
-];
-
-var dental = [
-    { hora: '8:30', especialista: 'ANDREA ZUÑIGA', paciente: 'MARCELA RETAMAL', rut: '11123425-6', prevision: 'ISAPRE' },
-    { hora: '11:00', especialista: 'MARIA PIA ZAÑARTU', paciente: 'ANGEL MUÑOZ', rut: '9878789-2', prevision: 'ISAPRE' },
-    { hora: '11:30', especialista: 'SCARLETT WITTING', paciente: 'MARIO KAST', rut: '7998789-5', prevision: 'FONASA' },
-    { hora: '13:00', especialista: 'FRANCISCO VON TEUBER', paciente: 'KARIN FERNANDEZ', rut: '18887662-K', prevision: 'FONASA' },
-    { hora: '13:30', especialista: 'EDUARDO VIÑUELA', paciente: 'HUGO SANCHEZ', rut: '17665461-4', prevision: 'FONASA' },
-    { hora: '14:00', especialista: 'RAQUEL VILLASECA', paciente: 'ANA SEPULVEDA', rut: '14441281-0', prevision: 'ISAPRE' },
-];
-
-//Agregar código para el desafio 2 aquí
-
 // Requerimiento UNO
-
 // 1. Agregar las siguientes horas al arreglo de Traumatología:
 let nuevasTraumatologia = (traumatologia.push(
     { hora: '09:00', especialista: 'RENÉ POBLETE', paciente: 'ANA GELLONA', rut: '13123329-7', prevision: 'ISAPRE' },
@@ -45,17 +13,47 @@ traumatologia.sort((a, b) => (parseInt(a.hora.split(':').join('')) > parseInt(b.
 console.table(traumatologia);
 
 // Requerimiento DOS
+// 2. Eliminar el primer y último elemento del arreglo de Radiología.
+radiologia.shift();
+radiologia.pop();
+console.table(radiologia);
+
+// Requerimiento TRES
+// 3. Imprimir la página, con document.write, la lista de pacientes de Dental, con los datos separados por guion, y cada fila separada por un párrafo.
+let imprimirLista = (lista) => {
+    let texto = '';
+
+    lista.forEach((paciente) => {
+        texto += `<p> ${paciente.hora} - ${paciente.especialista} - ${paciente.paciente} - ${paciente.rut} - ${paciente.prevision}</p>`;
+        return texto;;
+    });
+    return texto;
+}
+document.write('<h3>Lista pacientes Dental</h3>');
+document.write(imprimirLista(dental));
 
 
+// Requerimiento CUATRO
+// 4. Imprimir listado total de todos los pacientes que se atendieron.
+let total = traumatologia.concat(radiologia, dental);
+console.table(total);
 
+let imprimirNombres = (lista) => {
+    let arregloNombres = [];
+    lista.forEach((paciente) => {
+        arregloNombres.push(paciente.paciente);
+    });
+    arregloNombres.sort();
+    let texto = '';
+    arregloNombres.forEach((nombre) => {
+        texto += `<p>${nombre}</p>`;
+        ;
+    });
+    return texto;
+}
+console.log(total)
+document.write('<h3>Lista pacientes</h3>');
+document.write(imprimirNombres(total));
 
-//Agregar código para el desafio 2 aquí
-
-document.write(`<p>Cantidad de atenciones para Radiología: ${radiologia.length}</p>`);
-document.write(`<p>Cantidad de atenciones para Traumatología: ${traumatologia.length}</p>`);
-document.write(`<p>Cantidad de atenciones para Dental: ${dental.length}</p>`);
-
-
-document.write(`<p>Primera atencion: ${radiologia[0].paciente} - ${radiologia[0].prevision} | Última atención: ${radiologia[radiologia.length - 1].paciente} - ${radiologia[radiologia.length - 1].prevision}.</p>`);
-document.write(`<p>Primera atencion: ${traumatologia[0].paciente} - ${traumatologia[0].prevision} | Última atención: ${traumatologia[traumatologia.length - 1].paciente} - ${traumatologia[traumatologia.length - 1].prevision}.</p>`);
-document.write(`<p>Primera atencion: ${dental[0].paciente} - ${dental[0].prevision} | Última atención: ${dental[dental.length - 1].paciente} - ${dental[dental.length - 1].prevision}.</p>`);
+// Requerimiento CINCO
+//
